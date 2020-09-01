d154c117b66e3f9021212e59106e01951e959021
Mubra <46692684+Mubra@users.noreply.github.com>
2020-09-01 15:00:29 -0500

    Update settings.py

--- nearbyshops/nearbyshops/settings.py
+++ nearbyshops/nearbyshops/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kd0o&9_lt&3-yf(w2vq*xaox=6824u#*(jjj))mz#!%r^rgdni'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
